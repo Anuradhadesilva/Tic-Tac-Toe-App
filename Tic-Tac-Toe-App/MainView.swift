9b9c5cb68d5f34d27a9cdb16abd835c9547e3214
Anuradha Desilva <94860426+Anuradhadesilva@users.noreply.github.com>
2024-03-11 08:36:54 +0530

    Update MainView.swift

--- Tic-Tac-Toe-App/MainView.swift
+++ Tic-Tac-Toe-App/MainView.swift
@@ -5,11 +5,16 @@
 //  Created by Anuradha Desilva on 08/03/2024.
 //
 
+
 import SwiftUI
 
 struct MainView: View {
     @State var array = ["","","","","","","","",""]
     @State var count = 0
+    @State var showAlert = false
+    @State var isWin = false
+    @State var isDraw = false
+    @State var winner = ""
     @State private var selectedButton:Int?
     let winningSequences = [
            // Horizontal rows
@@ -21,38 +26,65 @@ struct MainView: View {
        ]
     var body: some View {
         VStack{
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3)){
-                ForEach(0...8, id: \.self){ index in
-                    VStack{
-                        Button{
-                            if count == 0 {
-                                array[index] = "X"
-                                count += 1
+            VStack{
+                Text("Tik Tak Toe Game")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundStyle(.white)
+                    .frame(minWidth: 0, maxWidth: .infinity ,minHeight:0 , maxHeight:60)
+                    .background(.black)
+                    .cornerRadius(8)
+            }
+            Spacer()
+            VStack{
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3)){
+                    ForEach(0...8, id: \.self){ index in
+                        VStack{
+                            Button{
+                                if count == 0 {
+                                    array[index] = "X"
+                                    count += 1
+                                }
+                                else if count == 1 {
+                                    array[index] = "O"
+                                    count -= 1
+                                }
+                                checkForWin()
+                            }label:{
+                                ZStack{
+                                    Rectangle()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                        .foregroundColor(.white)
+                                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 5)
+                                        .cornerRadius(8.0)
+                                    Text(array[index])
+                                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                        .foregroundColor(.black)
+                                }
+                                .padding(1)
                             }
-                            else if count == 1 {
-                                array[index] = "O"
-                                count -= 1
-                            }
-                            checkForWin()
-                        }label:{
-                            ZStack{
-                                Rectangle()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                                    .foregroundColor(.white)
-                                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 5)
-                                    .cornerRadius(8.0)
-                                Text(array[index])
-                                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                                    .foregroundColor(.black)
-                            }
-                            .padding(1)
+                            .disabled(!array[index].isEmpty)
+                            
                         }
-                        .disabled(array[index] == "X" || array[index] == "O")
-                        
+                        .alert(isPresented: $showAlert) {
+                            if isWin{
+                                Alert(title: Text(isWin ? "\(winner) is winner!" : "\(array[index]) Wins!"),
+                                      dismissButton: .default(Text("Ok")) {
+                                    resetGame()
+                                })
+                            } else{
+                                Alert(title: Text(isDraw ? "It's a Draw!" : "\(array[index]) Wins!"),
+                                      dismissButton: .default(Text("Ok")) {
+                                    resetGame()
+                                })
+                            }
+                        }
                     }
                 }
             }
+            Spacer()
+            
+            
         }
         .padding(.horizontal,20)
     }
@@ -62,13 +94,26 @@ struct MainView: View {
             let second = array[sequence[1]]
             let third = array[sequence[2]]
             if !first.isEmpty && first == second && second == third {
-                
+                showAlert = true
+                isWin = true
+                winner = first
                 print("\(first) wins!")
+                
                 // Add your logic for winning state here
+                return
+            }
+            if !array.contains(""){
+                showAlert = true
+                isDraw = true
                 return
             }
         }
     }
+    func resetGame() {
+            array = ["","","","","","","","",""]
+            count = 0
+            isWin = false
+        }
 }
 
 #Preview {
